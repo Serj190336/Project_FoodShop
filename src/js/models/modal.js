a10d551caa053a97626e7cc8f71cc8f639c964e5
Sergey Yusha <sergii190336@gmail.com>
2019-01-10 23:57:42 +0200

    added properties to Modal class, modal.js refactoring

--- src/js/models/modal.js
+++ src/js/models/modal.js
@@ -13,7 +13,10 @@ import { elements } from '../views/base'
       try {
         const recipeResponse = await axios(`http://food2fork.com/api/get?key=${key}&rId=${this.id}`);
         this.idCheck = recipeResponse;
+
+        this.title = recipeResponse.data.recipe.title;
         this.image = recipeResponse.data.recipe.image_url;
+        this.orderID = recipeResponse.data.recipe.recipe_id;
 
       } catch (error) {
         console.log(error);
@@ -32,3 +35,23 @@ import { elements } from '../views/base'
     }
 }
 
+// recipe:
+
+  // f2f_url: "http://food2fork.com/view/47746"
+  // image_url: "http://static.food2fork.com/best_pizza_dough_recipe1b20.jpg"
+
+  // ingredients: Array(6)
+    // 0: "4 1/2 cups (20.25 ounces) unbleached high-gluten, bread, or all-purpose flour, chilled"
+    // 1: "1 3/4 (.44 ounce) teaspoons salt"
+    // 2: "1 teaspoon (.11 ounce) instant yeast"
+    // 3: "1/4 cup (2 ounces) olive oil (optional)"
+    // 4: "1 3/4 cups (14 ounces) water, ice cold (40F)"
+    // 5: "Semolina flour OR cornmeal for dusting"
+    // length: 6
+
+  // publisher: "101 Cookbooks"
+  // publisher_url: "http://www.101cookbooks.com"
+  // recipe_id: "47746"
+  // social_rank: 100
+  // source_url: "http://www.101cookbooks.com/archives/001199.html"
+  // title: "Best Pizza Dough Ever"
--- src/js/views/modalView.js
+++ src/js/views/modalView.js
@@ -5,8 +5,6 @@ import {
 
 
 
-
-
 // Prepare modal
 export const createModal = () => {
   // add EventListener to every created link
@@ -17,37 +15,65 @@ export const createModal = () => {
       // 1) display (open) modal window
       elements.modalBlock.style.display = "block";
 
-      // 2) Create <span> x close button and remove results
+      // 2) Create <span> x close button and text
       elements.modalContent.innerHTML = `<span class="closemodal">&times;</span>
-      <p>Some text in the Modal..</p>`;
-      // Get the <span> element that closes the modal
-      // When the user clicks on <span> (x), close the modal
-      const closeModal = document.querySelector('.closemodal');
-      closeModal.onclick = function () {
-        elements.modalBlock.style.display = "none";
-      }
+      <p>Some text in the Modal..</p>`
 
-      // 3) Close modal when the user clicks anywhere outside of the modal
-      window.onclick = function (event) {
-        if (event.target == elements.modalBlock) {
-          elements.modalBlock.style.display = "none";
-        }
-      }
-      console.log('from modaljs')
+      // 3) onlick functions to close modal window
+      closeModalWindow();
 
+      // 4) 
 
-      //
       // Modal class and ID of clicked recipe
-        let modalState = {},
+      let modalState = {},
         datasetRecipeId = e.currentTarget.dataset.recipeid;
-        modalState = new Modal(datasetRecipeId);
-        // async send ID to api and get recipe info object
-        await modalState.recipeDetails();
-        console.log(modalState.image);
-        // elements.modalContent.innerHTML = `<span class="closemodal">&times;</span>
-        // <p>Some text in the Modal..</p>`;
+      modalState = new Modal(datasetRecipeId);
+      // async send ID to api and get recipe info object
+      await modalState.recipeDetails();
+      console.log(modalState.title);
+      console.log(modalState.image);
+      console.log(modalState.orderID);
+      // elements.modalContent.innerHTML = `<span class="closemodal">&times;</span>
+      // <p>Some text in the Modal..</p>`;
+
+
+
+      // elements.modalContent.innerHTML = `<span class="closemodal">&times;</span>
+      // <p>Some text in the Modal..</p>
+      // <p>${modalState.image}</p>`;
+
+
+
+
     })
   })
 }
 
-// Load info from API
+
+// Close modal and Clear results modal
+function closeModalWindow() {
+  // Get the <span> element that closes the modal
+  // Close modal when the user clicks on <span> (x)
+  let closeModal = document.querySelector('.closemodal');
+  console.log(closeModal);
+  closeModal.onclick = function () {
+    // hide modal window
+    console.log(closeModal);
+    elements.modalBlock.style.display = "none";
+    // clear results when close modal
+    elements.modalContent.innerHTML = `<span class="closemodal">&times;</span>
+          <p>Some text in the Modal..</p>`
+  }
+
+  // Close modal when the user clicks anywhere outside of the modal
+  window.onclick = function (event) {
+    if (event.target == elements.modalBlock) {
+      // hide modal window
+      elements.modalBlock.style.display = "none";
+      // clear results when close modal
+      elements.modalContent.innerHTML = `<span class="closemodal">&times;</span>
+          <p>Some text in the Modal..</p>`
+    }
+  }
+  console.log('from modaljs')
+}
